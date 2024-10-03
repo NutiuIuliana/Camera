1aa1a8fef442f3314c5fe068fe454d20328b6ae8
NutiuIuliana <131156371+NutiuIuliana@users.noreply.github.com>
2024-10-03 09:06:14 +0300

    Add files via upload

---
+++ camera.js
@@ -0,0 +1,26 @@
+var video = document.getElementById("video");
+
+video.addEventListener("touchstart", capteaza);
+video.addEventListener("mousedown", capteaza);
+
+function on_cam_success(stream) {
+    video.srcObject = stream;
+}
+
+function on_cam_error(err) {
+    alert("Eroare: " + err.message);
+}
+
+var constraints = { audio: false, video: true };
+
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(on_cam_success)
+    .catch(on_cam_error);
+
+function capteaza() {
+    var c = document.getElementById("canvas");
+    c.width = video.videoWidth;  
+    c.height = video.videoHeight;
+    var ctx = c.getContext("2d");
+    ctx.drawImage(video, 0, 0, c.width, c.height);
+}
